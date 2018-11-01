6bc1fbda944a5422eaefe982809fbc7fbfa9f7c9
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 21:15:10 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,9 +26,9 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT title, SUM(amount) - funding_goal AS amount_over_goal " \
   "FROM projects " \
-    "JOIN pledges ON project_id = project.id " \
+    "JOIN pledges ON project_id = projects.id " \
   "WHERE amount_over_goal >= 0 " \
-  "GROUP BY title"
+  "GROUP BY title "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
