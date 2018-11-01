a2632b1f367d7cd9d1ecafab212f164c18879fdb
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 21:08:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,9 +15,10 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT name, age, amount " \
+  "SELECT name, age, SUM(amount) " \
   "FROM users " \
     "JOIN pledges ON users.id = pledges.user_id " \
+  "GROUP BY name" \
   "ORDER BY name"
 end
 
